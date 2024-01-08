4ba03b5842c3d60b4c1883ae2734c3a31bd32ddf
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-01-08 19:08:25 +0530

    first commit

--- sample.js
+++ sample.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import { useState } from "react";
 
@@ -18,39 +17,21 @@ function App() {
         <input
           value={toDo}
           onChange={(e) => setToDo(e.target.value)}
-          type="text"
+          type="text" 
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() =>
-           { setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])
-            setToDo("")}
-          }
+          onClick={() => setToDos([...toDos, toDo])}
           className="fas fa-plus"
         ></i>
       </div>
       <div className="todos">
-        {toDos.map((obj) => {
+        {toDos.map((value) => {
           return (
             <div className="todo">
               <div className="left">
-                <input
-                  onChange={(e) => {
-                    setToDos(
-                      toDos.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj.status = e.target.checked;
-                        }
-                        return obj;
-                      })
-                    );
-                  }}
-                  value={obj.status}
-                  type="checkbox"
-                  name=""
-                  id=""
-                />
-                <p>{obj.text}</p>
+                <input type="checkbox" name="" id="" />
+                <p>{value}</p>
               </div>
               <div className="right">
                 <i className="fas fa-times"></i>
@@ -58,17 +39,6 @@ function App() {
             </div>
           );
         })}
-      {
-        toDos.map((obj)=>{
-          if (obj.status){
-            return (
-              <h1>{obj.text}</h1>
-            )
-          }
-          return null
-        })
-      }
-         
       </div>
     </div>
   );
